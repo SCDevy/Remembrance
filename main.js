799d0bdee25eabe07bac6f2b4f3696cb0264b41b
Stephen Chen <hello@stephenc.me>
2015-08-28 12:49:07 -0700

    Starts draw loop.

---
+++ main.js
@@ -0,0 +1,2 @@
+var canvas = document.getElementById("game-canvas");
+var game = Game(canvas, 20, 10);
--- remembrance.js
+++ remembrance.js
@@ -1,29 +1,41 @@
-var game = function(duration) {
-	var gameObject = {
-		score: 0,
+var Game = function(canvas, duration, interval) {
+	var Game = {
+		canvas: canvas,
 		duration: duration,
+		interval: interval,
+		score: 0,
+		x: 0,
+		y: 0,
 		initialize: function() {
-			this.gameLoop();
+			var ctx = this.canvas.getContext("2d");
+			setInterval(this.draw.bind(this, ctx), this.interval);
 		},
-		gameLoop: function() {
-			var choice = 
-			if(chec)
+		draw: function(ctx) {
+			ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, 10, 0, Math.PI*2);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+			this.x += 1;
+			this.y += 1;
 		},
 		getKeypress: function() {
-			
+
 		},
 		checkResponse: function(choice) {
-			
+
 		},
 		terminate: function() {
-			
+
 		}
 	};
-	gameObject.initialize();
-	return gameObject;
+	Game.initialize();
+	return Game;
 };
 
-var user = function(userName) {
+var User = function(userName) {
 	var userObject = {
 		name: userName,
 		highScore: 0,
