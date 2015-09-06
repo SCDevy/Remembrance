77e96b4f4f573acd42393ad939db4f347bd172d8
Stephen Chen <hello@stephenc.me>
2015-09-06 12:19:30 -0400

    Expands canvas to full viewport.

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+var vpWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var vpHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
 var canvas = document.getElementById("game-canvas");
+canvas.width = vpWidth;
+canvas.height = vpHeight;
+
 var game = Game(canvas, "#333333", ['red', 'green', 'blue', 'yellow'], 20, 10);
